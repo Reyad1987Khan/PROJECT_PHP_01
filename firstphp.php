2344129163d13a110ca4b8c7a53506bb29369d18
Reyad Khan <khanreyad207@gmail.com>
2022-11-27 01:10:26 +0600

    comments

--- firstphp.php
+++ firstphp.php
@@ -1,20 +1,22 @@
 <?php
-    $marks = 999;
-    if ($marks >= 80 && $marks <= 100){
+    $result = 999;
+
+if ($result >= 80 and $result <= 100) {
         echo "You got A+";
-    }else if ( $marks >= 70 && $marks <= 79) {
-        echo "You got A";
-    }else if ( $marks >=60  && $marks <= 69) {
-        echo "You got A-";
-    }else if ( $marks >= 50 && $marks <= 59) {
-        echo "You got B";
-    }else if ( $marks >= 40 && $marks <= 49) {
-        echo "You got C";
-    }else if ( $marks >= 33 && $marks <= 39) {
-        echo "You got D";
-    }else if ( $marks >= 0 && $marks <= 32) {
-        echo "You got F";
-    }else {
-        echo "You don't deserv good for somthing";
-    }
-?>
+} else if ($result >= 70 && $result <= 79) { 
+        echo "You got A Grad";
+}else if ($result >= 60 && $result <= 69) { 
+    echo "You got A- Grad";
+}else if ($result >= 50 && $result <= 59) { 
+    echo "You got B Grad";
+}else if ($result >= 40 && $result <= 49) { 
+    echo "You got C Grad";
+}else if ($result >= 33 && $result <= 49) { 
+    echo "You got D Grad";
+}else if ($result >= 0 && $result <= 32) { 
+    echo "You got F Grad";
+}else { 
+    echo "You got nothing";
+}
+
+ ?>
